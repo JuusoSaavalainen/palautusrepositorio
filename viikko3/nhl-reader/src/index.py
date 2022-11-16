2fb273efe74462d15a8786f7ab3ddd35b7616bed
saavajuu <juuso.saavalainen@helsinki.fi>
2022-11-16 15:29:03 +0200

    vk3 t3

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -5,3 +5,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#tähän olisi kai kans voinu tehdä ton homman mut
+#se löytyneepi projektipohja.py tiedostosta :)
--- viikko3/nhl-reader/src/projektipohja.py
+++ viikko3/nhl-reader/src/projektipohja.py
@@ -2,38 +2,45 @@ import requests
 from datetime import datetime
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        
+    def get_players(self):
+        response = requests.get(self.url).json()
+        players = []
+        for player_dict in response:
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists'],
+                player_dict['nationality'],
+                player_dict['penalties'],
+                player_dict['games']
+            )
+            players.append(player)
+        return players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader.get_players()
+
+    def top_scorers_by_nationality(self,nat):
+        player_list = []
+        print(f'Players from {nat}',datetime.now())
+        self.reader.sort(key=lambda x: x.goals + x.assists, reverse=True)
+        for player in self.reader:
+            if player.nationality == nat:
+                player_list.append(player)
+        return player_list
+
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        player = Player(
-            player_dict['name'],
-            player_dict['team'],
-            player_dict['goals'],
-            player_dict['assists'],
-            player_dict['nationality'],
-            player_dict['penalties'],
-            player_dict['games']
-        )
-
-        players.append(player)
-
-
-#standart str kutsun palautus 
-    print("Oliot:")
     for player in players:
         print(player)
-
-#pelaajat suomalaisittain
-    players.sort(key=lambda x: x.goals + x.assists, reverse=True)
-    print('Players from FIN',datetime.now())
-    for player in players:
-        if player.nationality == "FIN":
-            print(player)
 main()
