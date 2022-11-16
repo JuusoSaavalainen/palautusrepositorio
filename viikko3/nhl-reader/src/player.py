43adab2e3bf0f0ca5ffad84ea8ec4a61785578c5
saavajuu <juuso.saavalainen@helsinki.fi>
2022-11-16 14:44:12 +0200

    vk3 t2

--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,9 +1,13 @@
 class Player:
-    def __init__(self, name, team, goals, assists):
+    def __init__(self, name, team, goals, assists, nationality, games, penalties):
         self.name = name
         self.team = team
         self.goals = goals
         self.assists = assists
+        self.nationality = nationality
+        self.penalties = penalties
+        self.games = games
 
     def __str__(self):
-        return (f"{self.name} team {self.team} goals {self.goals} assists {self.assists}")
+        
+        return (f"{self.name:21} {self.team:^3} {self.goals:^3}+ {self.assists:^3}={self.goals+self.assists:>3}")
--- viikko3/nhl-reader/src/projektipohja.py
+++ viikko3/nhl-reader/src/projektipohja.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 from player import Player
 
 def main():
@@ -15,13 +16,24 @@ def main():
             player_dict['name'],
             player_dict['team'],
             player_dict['goals'],
-            player_dict['assists']
+            player_dict['assists'],
+            player_dict['nationality'],
+            player_dict['penalties'],
+            player_dict['games']
         )
 
         players.append(player)
 
-    print("Oliot:")
 
+#standart str kutsun palautus 
+    print("Oliot:")
     for player in players:
         print(player)
+
+#pelaajat suomalaisittain
+    players.sort(key=lambda x: x.goals + x.assists, reverse=True)
+    print('Players from FIN',datetime.now())
+    for player in players:
+        if player.nationality == "FIN":
+            print(player)
 main()
