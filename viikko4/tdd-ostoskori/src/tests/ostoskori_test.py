537d1c94a96203d7eb35bcaee886bc42106377ec
saavajuu <juuso.saavalainen@helsinki.fi>
2022-11-28 16:40:32 +0200

    testi lisätty kahden saman tuotteen lisäämisen jälkeiseen tilanteeseen; nimi ja määrä oikeat

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -94,8 +94,15 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito) 
-        self.kori.lisaa_tuote(maito) 
         ostos = self.kori.ostokset()
 
         self.assertEqual(len(ostos), 1)
     
+    #step 12 
+    def test_kahden_saman_tuotteen_lisäämisen_jälkeen_ostoskori_sisältää_ostoksen_jolla_sama_nimi_kuin_tuotteella_ja_lukumäärä_2(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito) 
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 2)
